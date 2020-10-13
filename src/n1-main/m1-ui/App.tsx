c174160d0bfa9b00f27038a74a53ec5d3bf6087e
Prough123 <slipok1999@mail.ru>
2020-10-13 13:52:37 +0300

    init

---
+++ src/n1-main/m1-ui/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+                {/*hr , provider*/}
+                <>
+                    {/*/!*<Header/>*!/ navLink*/}
+
+
+                </>
+        </div>
+    );
+};
+
+export default App;
